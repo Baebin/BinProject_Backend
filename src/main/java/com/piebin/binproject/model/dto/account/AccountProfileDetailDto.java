00ee950b2c9a524bc0af8bff7b39189d9399f9dc
Baebin <baebine@naver.com>
2024-08-10 23:51:17 +0900

    Dto Edited

--- src/main/java/com/piebin/binproject/model/dto/account/AccountProfileDetailDto.java
+++ src/main/java/com/piebin/binproject/model/dto/account/AccountProfileDetailDto.java
@@ -1,10 +1,14 @@
 package com.piebin.binproject.model.dto.account;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.piebin.binproject.model.domain.Account;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -16,6 +20,9 @@ public class AccountProfileDetailDto {
     private String name;
     private String phone;
     private String email;
+    @JsonFormat(pattern = "yyyy.MM.dd HH:mm")
+    @JsonProperty("reg_date")
+    private LocalDateTime regDate;
 
     public static AccountProfileDetailDto toDto(Account account) {
         return AccountProfileDetailDto.builder()
@@ -24,6 +31,7 @@ public class AccountProfileDetailDto {
                 .name(account.getName())
                 .phone(account.getPhone())
                 .email(account.getEmail())
+                .regDate(account.getRegDate())
                 .build();
     }
 }
