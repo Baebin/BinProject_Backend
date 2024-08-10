ce36d5af981fcfa71ca93cabfc5ac10335ecb2b6
Baebin <baebine@naver.com>
2024-08-10 18:24:11 +0900

    Initial commit

---
+++ src/main/java/com/piebin/binproject/BinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.piebin.binproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinProjectApplication.class, args);
+    }
+
+}
